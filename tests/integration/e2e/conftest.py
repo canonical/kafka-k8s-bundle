b47b7cff4ea662389c05b5185ec6ad9befc22c3d
Enrico Deusebio <enrico.deusebio@canonical.com>
2025-02-14 08:53:39 +0100

    fix linting

--- tests/integration/e2e/conftest.py
+++ tests/integration/e2e/conftest.py
@@ -116,11 +116,7 @@ async def deploy_cluster(ops_test: OpsTest, bundle_file):
 
     async with ops_test.fast_forward(fast_interval="30s"):
         await ops_test.model.wait_for_idle(
-            apps=apps,
-            idle_period=20,
-            status="active",
-            timeout=1000,
-            raise_on_error=False
+            apps=apps, idle_period=20, status="active", timeout=1000, raise_on_error=False
         )
 
     logger.info("Bundle deployed!")
