35e60dfbfdf3b0036b9f5c90dd236fe38ed90cef
Enrico Deusebio <enrico.deusebio@canonical.com>
2025-02-14 19:54:55 +0100

    improve test

--- tests/integration/e2e/test_basic_flow.py
+++ tests/integration/e2e/test_basic_flow.py
@@ -190,6 +190,24 @@ async def test_test_app_actually_set_up(
     # await ops_test.model.wait_for_idle(apps=[consumer], status="active", timeout=1000)
     # logger.info("End scale down")
 
+    # Stop producers first
+    await ops_test.model.applications[producer].remove_relation(
+        f"{producer}:kafka-cluster", f"{kafka}"
+    )
+    await ops_test.model.wait_for_idle(
+        apps=[producer], idle_period=30, status="blocked", timeout=600
+    )
+
+
+    # Then stop consumers
+    await ops_test.model.applications[consumer].remove_relation(
+        f"{consumer}:kafka-cluster", f"{kafka}"
+    )
+
+    await ops_test.model.wait_for_idle(
+        apps=[consumer], idle_period=30, status="blocked", timeout=600
+    )
+
     # destroy producer and consumer during teardown.
     logger.info("End of the test!")
 
