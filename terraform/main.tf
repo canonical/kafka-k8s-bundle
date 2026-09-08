1c0b8ec1225e5ab2d01f5c9386275c806dd7d7ca
Raul Zamora <raul.zamora@canonical.com>
2026-09-08 18:07:15 +0200

    add oauth interface

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,7 @@ locals {
   ui_app_name         = var.ui.units > 0 ? module.ui[0].app_name : null
   cos_enabled         = var.cos_offers.dashboard != null ? true : false
   tls_enabled         = var.tls_offer != null ? true : false
+  oauth_enabled       = var.oauth_offer != null ? true : false
   cos_agent_charm     = "opentelemetry-collector-k8s"
   cos_agent_channel   = "2/stable"
 }
